78cc4edd9645ed171308f1ca908a0b4119b913c6
Christina O'Donnell <cdo@mutix.org>
2023-12-29 14:17:27 +0000

    Refactor build scripts

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@
 
 rm -rf out/*
 emacs -Q --script build.el
+cp out/octocurious/pages/index.html out/octocurious/index.html
+#ln -s out/mutix/pages/index.html out/mutix/index.html
--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@
 
 server=algernon
 
-rm -rf out/*
-emacs -Q --script build.el
+./build.sh
 ssh $server -- rm -rf /var/www/*
 scp -r out/* $server:/var/www/
