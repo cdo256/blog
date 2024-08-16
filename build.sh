3d269a86a5ce3c099bcc19162a19f784ebbac0e4
Christina O'Donnell <cdo@mutix.org>
2024-08-16 22:29:17 +0100

    Simplify build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "==== Remove previous build artifacts ===="
-rm -rf ./content
-rm -rf ./public/
-echo ""
-
 echo "==== Generate Hugo markdown using ox-hugo ===="
 emacs -Q --script build.el
 echo ""
@@ -14,6 +9,4 @@ echo "==== Build with Hugo ===="
 hugo --ignoreCache -D
 echo ""
 
-echo "==== Copy .well-known for webfinger ===="
-cp ./.well-known -r ./public/
-echo ""
+echo "==== SUCCESS ===="
